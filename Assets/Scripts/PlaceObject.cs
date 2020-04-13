30ace754e41ea2547ad3d5d33773695cd937a867
agentvm <agentvm@users.noreply.github.com>
2020-04-13 15:33:08 +0000

    Make placed objects face the camera

--- Assets/Scripts/PlaceObject.cs
+++ Assets/Scripts/PlaceObject.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlaceObject : MonoBehaviour
 {
     [SerializeField] private Object objectToInstantiate;
+    private Camera mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        InputModule.Instance.functionsOnPlaneTouch.Add (Place);
+        InputModule.Instance.SubscribeFunctionToPlaneTouch (Place);
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
@@ -18,10 +20,17 @@ public class PlaceObject : MonoBehaviour
         
     }
 
+    // Make a new Instance of the object assigned in the Inspector, place it at the given position and rotate it towards the camera
     void Place (Vector3 position)
     {
+        // instantiate
         if ( !objectToInstantiate ) return;
+        Transform instantiatedObject = ((GameObject)Instantiate (objectToInstantiate, position, new Quaternion (), this.transform )).transform;
 
-        Instantiate (objectToInstantiate, position, new Quaternion (), this.transform );
+        // rotate towards camera
+        if ( !mainCamera ) return;
+
+        float heightDifference = mainCamera.transform.position.y - instantiatedObject.position.y;
+        instantiatedObject.LookAt (mainCamera.transform.position - Vector3.up * heightDifference);
     }
 }
