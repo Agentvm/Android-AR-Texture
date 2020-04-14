b69addda87f52508280654502b2d6e9f8542c44c
agentvm <agentvm@users.noreply.github.com>
2020-04-14 13:40:11 +0000

    Add non-functional drawing prototype
    Basically a camera strapped to a brush that is placed over objects and renders once on click or touch.

---
+++ Assets/Scripts/Draw.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw : MonoBehaviour
+{
+    [SerializeField] private Camera brushRenderCamera;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        InputModule.Instance.SubscribeToTouch (drawOnClick);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+
+    public void drawOnClick ( RaycastHit raycastHit )
+    {
+        // check if hit object can be drawn on
+        if ( raycastHit.transform.tag != "Drawable" && raycastHit.transform.tag != "Plane" )
+
+        // make the render camera face the object
+        //brushRenderCamera.transform.rotation = Quaternion.Euler (-raycastHit.normal);
+        brushRenderCamera.transform.position = raycastHit.normal.normalized * 1.5f - raycastHit.point;
+        brushRenderCamera.transform.LookAt (raycastHit.point);
+    }
+}
---
+++ Assets/Scripts/RenderDrawings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderDrawings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void RenderFrame (Texture texture)
+    {
+
+    }
+}
