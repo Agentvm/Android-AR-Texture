1f36705ba00745b7d7883e6f280eb35b543cf54a
agentvm <agentvm@users.noreply.github.com>
2020-04-20 17:40:37 +0000

    Remove old, overpainted brushes on planes

--- Assets/Scripts/Draw/BrushColor.cs
+++ Assets/Scripts/Draw/BrushColor.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class BrushColor : MonoBehaviour
 {
+    // Components
     Renderer brushRenderer;
+
+    // Color
     [SerializeField] Material greenBrushMaterial;
     [SerializeField] Material yellowBrushMaterial;
     [SerializeField] Material redBrushMaterial;
-
     Color red, green, yellow;
+
+    // Old Brushes may be removed to improve performance
+    float placementTime;
+
+    // Property
+    public float PlacementTime { get => placementTime; }
 
     private void Awake ()
     {
@@ -20,21 +28,12 @@ public class BrushColor : MonoBehaviour
     void Start()
     {
         brushRenderer = this.GetComponent<Renderer> ();
-        
-        //// r
-        //red.r = 0f;
-        //red.g = 180f;
-        //red.b = 0f;
+        placementTime = Time.time;
+    }
 
-        //// g
-        //green.r = 180f;
-        //green.g = 0f;
-        //green.b = 0f;
-
-        //// y
-        //yellow.r = 0f;
-        //yellow.g = 180f;
-        //yellow.b = 180f;
+    public void ResetPlacementTime ()
+    {
+        placementTime = Time.time;
     }
 
     public void TurnGreen ()
@@ -51,23 +50,4 @@ public class BrushColor : MonoBehaviour
     {
         brushRenderer.material = yellowBrushMaterial;
     }
-
-    //public void TurnGreen ()
-    //{
-    //    //brushRenderer.material = greenBrushMaterial;
-    //    brushRenderer.material.SetColor ("_Color", green);
-    //    brushRenderer.material.SetColor ("_EmissionColor", green);
-    //}
-
-    //public void TurnRed ()
-    //{
-    //    brushRenderer.material.SetColor ("_Color", red);
-    //    brushRenderer.material.SetColor ("_EmissionColor", red);
-    //}
-
-    //public void TurnYellow ()
-    //{
-    //    brushRenderer.material.SetColor ("_Color", yellow);
-    //    brushRenderer.material.SetColor ("_EmissionColor", yellow);
-    //}
 }
--- Assets/Scripts/Draw/RenderDrawings.cs
+++ Assets/Scripts/Draw/RenderDrawings.cs
@@ -78,13 +78,14 @@ public class RenderDrawings : MonoBehaviour
             Transform placedBrush = brushPool.RemoveElementFromPool (raycastHit.point + new Vector3 (0f, 0.1f, 0f), hitObjectTransform.rotation);
             placedBrush.tag = "Drawing";
             placedBrush.localScale = new Vector3 (planeBrushScale, planeBrushScale, planeBrushScale);
+            placedBrush.GetComponent<BrushColor> ().ResetPlacementTime ();
 
             // Change brush color
             List<Transform> taggedNodes = new List<Transform> ();
             foreach ( GameObject gO in GameObject.FindGameObjectsWithTag ("Drawing") )
                 taggedNodes.Add (gO.GetComponent<Transform> ());
 
-            determineBrushColor (placedBrush, taggedNodes);
+            determineBrushColor (placedBrush, taggedNodes, true);
 
             // Hide, if heatmap is not shown
             if ( !GameState.Instance.HeatmapActive )
@@ -114,6 +115,7 @@ public class RenderDrawings : MonoBehaviour
     {
         // set brush position to clicked uv coordinates (uv = screen: top-left (0,1), top-right (1,1), bottom-left (0,0), bottom-right (1,0))
         Vector3 brushPosition = renderCamera.ViewportToWorldPoint (new Vector3 (uvCoordinates.x, uvCoordinates.y, 0.9f));
+        Transform instantiatedBrush = null;
 
         // Check if the object has been painted on before
         if ( HeatmapConfigurations.Count == 0 ) // no painting done at all
@@ -122,9 +124,7 @@ public class RenderDrawings : MonoBehaviour
             HeatmapConfigurations.Add (touchedObject, new HeatmapConfiguration (brushPosition, OriginalRenderTexture));
 
             // Instantiate a new brush at the freshly clicked Position
-            Transform instantiatedBrush = brushPool.InstantiateElementFromPool (brushPosition);
-            determineBrushColor (instantiatedBrush, brushPool.ActiveElements);
-            instantiatedBrush.localScale = new Vector3 (brushScale, brushScale, brushScale);
+            instantiatedBrush = brushPool.InstantiateElementFromPool (brushPosition);
         }
         else if ( !HeatmapConfigurations.ContainsKey (touchedObject) ) // Object not yet known
         {
@@ -133,10 +133,9 @@ public class RenderDrawings : MonoBehaviour
 
             // Add a new HeatmapConfiguration containing the touched UV coordinate as first position
             HeatmapConfigurations.Add (touchedObject, new HeatmapConfiguration (brushPosition, OriginalRenderTexture));
-            
-            Transform instantiatedBrush = brushPool.InstantiateElementFromPool (brushPosition);
-            determineBrushColor (instantiatedBrush, brushPool.ActiveElements);
-            instantiatedBrush.localScale = new Vector3 (brushScale, brushScale, brushScale);
+
+            // Instantiate a new brush at the freshly clicked Position
+            instantiatedBrush = brushPool.InstantiateElementFromPool (brushPosition);
         }
         else if ( lastActiveObject && lastActiveObject != touchedObject ) // we know this object
         {
@@ -150,9 +149,7 @@ public class RenderDrawings : MonoBehaviour
             // reposition the active brushes to fit the new Configuration or get inactive brushes from the pool
             foreach ( Vector3 storedBrushPosition in restoredBrushPositions )
             {
-                Transform instantiatedBrush = brushPool.InstantiateElementFromPool (storedBrushPosition);
-                determineBrushColor (instantiatedBrush, brushPool.ActiveElements);
-                instantiatedBrush.localScale = new Vector3 (brushScale, brushScale, brushScale);
+                instantiatedBrush = brushPool.InstantiateElementFromPool (storedBrushPosition);
             }
         }
         else // this is the object we clicked on before
@@ -160,31 +157,62 @@ public class RenderDrawings : MonoBehaviour
             // Add the newest touch to the known positions and
             // Instantiate a new brush at the freshly clicked Position
             HeatmapConfigurations[touchedObject].AddBrushPosition (brushPosition);
-            Transform instantiatedBrush = brushPool.InstantiateElementFromPool (brushPosition);
-            determineBrushColor (instantiatedBrush, brushPool.ActiveElements);
-            instantiatedBrush.localScale = new Vector3 (brushScale, brushScale, brushScale);
+            instantiatedBrush = brushPool.InstantiateElementFromPool (brushPosition);
         }
+
+        // Scale the instantiated brush and change its color, if appropriate
+        determineBrushColor (instantiatedBrush, brushPool.ActiveElements);
+        instantiatedBrush.localScale = new Vector3 (brushScale, brushScale, brushScale);
+
     }
 
     // According to the proximity to placed brushes, recolor the newly placed brush
-    void determineBrushColor (Transform brush, List<Transform> nearbyBrushes)
+    void determineBrushColor (Transform brush, List<Transform> nearbyBrushes, bool removeUnderlyingBrushes = false)
     {
+        // Color Change Variables
         BrushColor brushColorScriptReference = brush.GetComponent<BrushColor> ();
         int neighborsInThreshold = 0;
 
-        // count neighbors in threshold
-        foreach (Transform activeBrushTransform in nearbyBrushes )
+        // Destroy old brushes variables
+        Transform oldestBrush = null;
+        float oldestBrushCreationTime = float.PositiveInfinity;
+
+        // Count neighbors in threshold
+        foreach (Transform brushTransform in nearbyBrushes )
         {
             // Compute vector diff
-            Vector3 vectorDifference = (brush.position - activeBrushTransform.position);
+            Vector3 vectorDifference = (brush.position - brushTransform.position);
 
             // Determine touch distance by using magnitude (is this subject to camera scale?)
             if ( vectorDifference.magnitude < tapDistanceTreshold )
-                neighborsInThreshold++;
+            {
+                neighborsInThreshold++; // there is one more neighbor around
+
+                // Remove old brushes in smaller area
+                if ( removeUnderlyingBrushes && vectorDifference.magnitude < tapDistanceTreshold / 2 )
+                {
+                    // Get Creation Time
+                    float currentBrushCreationTime = brushTransform.GetComponent<BrushColor> ().PlacementTime;
+
+                    // Find oldest brush
+                    if ( currentBrushCreationTime < oldestBrushCreationTime )
+                    {
+                        oldestBrush = brushTransform;
+                        oldestBrushCreationTime = currentBrushCreationTime;
+                    }
+                }
+            }
         }
 
         // Set material according to number of neighbors
-        if ( neighborsInThreshold >= tapQuantityTreshold * 2 ) brushColorScriptReference.TurnRed ();
+        if ( neighborsInThreshold >= tapQuantityTreshold * 2 )
+        {
+            brushColorScriptReference.TurnRed ();
+
+            // Remove oldest brush, it probably isn't even visible anymore
+            if ( removeUnderlyingBrushes && oldestBrush )
+                Destroy (oldestBrush.gameObject);
+        }
         else if (neighborsInThreshold >= tapQuantityTreshold ) brushColorScriptReference.TurnYellow ();
         else brushColorScriptReference.TurnGreen ();
     }
