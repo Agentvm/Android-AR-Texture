9dba9f864b524cbf769ec977b7a7ebbdd1542aeb
agentvm <agentvm@users.noreply.github.com>
2020-04-20 11:04:39 +0000

    Make Brushes more visible by adding Emission

--- Assets/ToDo.cs
+++ Assets/ToDo.cs
@@ -2,19 +2,16 @@
  * 
 
  ToDo:
-    Block Raycasts when the UI was touched
-    Reduce Raycast max Lenght
-    Make Brushes more visible
     Stop Recoloring of objects on first touch
     (Make brush scale with object size)
-
-
-
+    Reduce Raycast max Lenght
 
  Done:
-     preload delete on update fix
-     delegate logic placed in InputModule fix
-     Newly created Gameobjects don't care about the Heatmap Setting
+    Make Brushes more visible
+    Block Raycasts when the UI was touched
+    preload delete on update fix
+    delegate logic placed in InputModule fix
+    Newly created Gameobjects don't care about the Heatmap Setting
 
  * 
  * */
